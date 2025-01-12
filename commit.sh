07808cc1784bdbfb1dedaeab531d746211d111cc
Andre V Silva <andsilvadrcc@gmail.com>
2025-01-12 09:41:08 -0300

    my webpage

--- commit.sh
+++ commit.sh
@@ -6,7 +6,7 @@ echo "Add changes in files or directory..."
 git add .
 echo "committing..."
 git commit -m "${1}"
-git pull origin master
-git push -u origin master
+git pull origin main
+git push -u origin main
 echo "All done! :)"
 
